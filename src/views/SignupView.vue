bc2291bddfc85b7a1c992b320a3a81fb921c9eba
ryanmc954 <ryanmc2009@hotmail.co.uk>
2023-03-23 18:23:54 +0000

    added sign up page

--- src/views/SignupView.vue
+++ src/views/SignupView.vue
@@ -1,11 +1,23 @@
 <template>
     <div>
         <!-- Sign up page -->
-       
+        <UserForm formType="signup" @submit="signup" />
     </div>
 </template>
 
 <script setup>
-
+import UserForm from '../components/UserForm.vue';
+function signup (){
+alert("You clicked me")
+client.post('/signup', {
+    user: user
+  })
+    .then(res => {
+      console.log("res data", res)
+    })
+    .catch(errors =>{
+        console.log("error", errors)
+    })
+}
 </script>
 
